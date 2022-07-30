e2ef3071dd2638aba8fa7685ff8f90410155827e
Nguyễn Bắc <nguyentienbac.ult@gmail.com>
2022-07-30 18:13:44 +0700

    2022/07/30

--- HttpRq/Form1.Designer.cs
+++ HttpRq/Form1.Designer.cs
@@ -30,11 +30,18 @@
         {
             this.btnCrawl = new System.Windows.Forms.Button();
             this.txtLink = new System.Windows.Forms.TextBox();
+            this.btnSampleLink = new System.Windows.Forms.Button();
+            this.btnChangeIP = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnUpFile = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPasswd = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnCrawl
             // 
-            this.btnCrawl.Location = new System.Drawing.Point(43, 151);
+            this.btnCrawl.Location = new System.Drawing.Point(13, 114);
             this.btnCrawl.Name = "btnCrawl";
             this.btnCrawl.Size = new System.Drawing.Size(75, 23);
             this.btnCrawl.TabIndex = 0;
@@ -44,20 +51,89 @@
             // 
             // txtLink
             // 
-            this.txtLink.Location = new System.Drawing.Point(43, 45);
+            this.txtLink.Location = new System.Drawing.Point(13, 14);
             this.txtLink.Name = "txtLink";
-            this.txtLink.Size = new System.Drawing.Size(425, 23);
+            this.txtLink.Size = new System.Drawing.Size(245, 23);
             this.txtLink.TabIndex = 1;
+            // 
+            // btnSampleLink
+            // 
+            this.btnSampleLink.Location = new System.Drawing.Point(264, 13);
+            this.btnSampleLink.Name = "btnSampleLink";
+            this.btnSampleLink.Size = new System.Drawing.Size(108, 23);
+            this.btnSampleLink.TabIndex = 2;
+            this.btnSampleLink.Text = "Sample Link";
+            this.btnSampleLink.UseVisualStyleBackColor = true;
+            this.btnSampleLink.Click += new System.EventHandler(this.btnSampleLink_Click);
+            // 
+            // btnChangeIP
+            // 
+            this.btnChangeIP.Location = new System.Drawing.Point(264, 42);
+            this.btnChangeIP.Name = "btnChangeIP";
+            this.btnChangeIP.Size = new System.Drawing.Size(108, 23);
+            this.btnChangeIP.TabIndex = 3;
+            this.btnChangeIP.Text = "Change IP";
+            this.btnChangeIP.UseVisualStyleBackColor = true;
+            this.btnChangeIP.Click += new System.EventHandler(this.btnChangeIP_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(13, 43);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(245, 23);
+            this.txtIP.TabIndex = 4;
+            // 
+            // btnUpFile
+            // 
+            this.btnUpFile.Location = new System.Drawing.Point(94, 114);
+            this.btnUpFile.Name = "btnUpFile";
+            this.btnUpFile.Size = new System.Drawing.Size(75, 23);
+            this.btnUpFile.TabIndex = 5;
+            this.btnUpFile.Text = "Up File";
+            this.btnUpFile.UseVisualStyleBackColor = true;
+            this.btnUpFile.Click += new System.EventHandler(this.btnUpFile_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(175, 114);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(13, 72);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 23);
+            this.txtUser.TabIndex = 7;
+            // 
+            // txtPasswd
+            // 
+            this.txtPasswd.Location = new System.Drawing.Point(158, 72);
+            this.txtPasswd.Name = "txtPasswd";
+            this.txtPasswd.Size = new System.Drawing.Size(100, 23);
+            this.txtPasswd.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 211);
+            this.ClientSize = new System.Drawing.Size(387, 149);
+            this.Controls.Add(this.txtPasswd);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnUpFile);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnChangeIP);
+            this.Controls.Add(this.btnSampleLink);
             this.Controls.Add(this.txtLink);
             this.Controls.Add(this.btnCrawl);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,5 +143,12 @@
 
         private Button btnCrawl;
         private TextBox txtLink;
+        private Button btnSampleLink;
+        private Button btnChangeIP;
+        private TextBox txtIP;
+        private Button btnUpFile;
+        private Button btnLogin;
+        private TextBox txtUser;
+        private TextBox txtPasswd;
     }
 }
--- HttpRq/Form1.cs
+++ HttpRq/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Net;
 using xNet;
 namespace HttpRq
 {
@@ -7,11 +9,100 @@ namespace HttpRq
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtIP.Text = GetIPPublic();     
+        }
+
+
+
+        private string GetIPPublic()
+        {
+            string externalIpString = new WebClient().DownloadString("http://icanhazip.com").Replace("\\r\\n", "").Replace("\\n", "").Trim();
+            var externalIp = IPAddress.Parse(externalIpString);
+            return externalIp.ToString();
+        }
+
+        void TestData(string html)
+        {
+            File.WriteAllText("res.html", html);
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo("res.html")
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+        }
+
+        string GetData()
+        {
+            HttpRequest http = new HttpRequest();
+            http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36";
+            string html = http.Get(txtLink.Text).ToString();
+            return html;
+        }
+
+        string GetData(string link)
+        {
+            HttpRequest http = new HttpRequest();
+            http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36";
+            string html = http.Get(link).ToString();
+            return html;
+        }
 
         private void btnCrawl_Click(object sender, EventArgs e)
         {
+            var html = GetData();
+            TestData(html);
+        }
+
+        private void btnSampleLink_Click(object sender, EventArgs e)
+        {
+            string link = "https://mbasic.facebook.com/";
+            txtLink.Text = link;
+        }
+
+        private void btnChangeIP_Click(object sender, EventArgs e)
+        {
             HttpRequest http = new HttpRequest();
-                
+            http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
+            string ip = "45.235.128.6:60949";
+
+            //http.Password = "";
+            //http.Username = "";
+
+            //Socks4ProxyClient.Parse(ip);
+            //Socks5ProxyClient.Parse(ip);
+            http.Proxy = HttpProxyClient.Parse(ip);
+
+            var html = GetData("https://whoer.net/");
+            TestData(html);
+        }
+
+        private void btnUpFile_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                //UploadFile(dialog.FileName); waiting update database model
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            HttpRequest http = new HttpRequest();
+
+            http.Cookies = new CookieDictionary();
+
+            string linkLogin = "https://mbasic.facebook.com/login/?email=s&next=https://mbasic.facebook.com/home.php&li=QOzkYiMcjZ_IZGq0Bw-haYzr&e=1348028&shbl=1&refsrc=deprecated&_rdr";
+
+            http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36";
+
+            string data = "lsd=AVqw9qDIJ-I&jazoest=2886&m_ts=1659168891&li=e-jkYuDGdjfMKXD2nqG-fJ3u&try_number=0&unrecognized_tries=0&email=bacult1990%40gmail.com&pass=bac22198&login=%C4%90%C4%83ng+nh%E1%BA%ADp&bi_xrwh=0";
+
+            string html = http.Post(linkLogin, data, "text/html; charset=utf-8").ToString(); ;
+
         }
     }
 }
